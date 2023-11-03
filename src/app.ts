31d12e3fdf8b97f3ba6bcb9a7415bb37669dc773
Samreen8888 <139248829+Samreen8888@users.noreply.github.com>
2023-11-03 12:48:18 -0700

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,76 @@
+import * as inquirer from "inquirer";
+import chalk from "chalk";
+
+// calculator operator
+
+enum operator
+
+{
+    ADD = "Addition",
+    SUBTRACT = "Subtraction",
+    MULTIPLY= "multiplication",
+    DIVIDE = "Division",
+
+}
+
+const prompt = inquirer.createPromptModule();
+
+function validateNumber(input: string): boolean | string 
+{ if (isNaN(parseFloat(input)))
+    {
+        return "Please enter a valid number"
+    }
+    return true;
+}
+
+async function main() 
+{
+const input = await prompt([
+    {
+    type: "input",
+    name: "num1",
+    message: "Please enter the first number:",
+    validate: validateNumber,
+},
+{
+    type: "rawlist", //or can use list;
+    name: "operator",
+    message: "Slect an operation:",
+    choices: Object.values(operator)
+},
+{
+    type: "input",
+    name: "num2",
+    message: "Please enter the second number:",
+    validate: validateNumber,
+
+}
+
+])
+
+const num1 = parseFloat(input.num1);
+const num2 = parseFloat(input.num2);
+const selectedOperator = input.operator as operator.ADD;
+let result; Number;
+
+if (selectedOperator === operator.ADD)
+{
+    result = num1 + num2;
+    console.log(chalk.green.bgRedBright(`Result is : ${result}`))
+}else if (selectedOperator === operator.SUBTRACT)
+{
+    result = num1 - num2;
+    console.log(chalk.yellow.bgRedBright(`Result is : ${result}`))
+}else if (selectedOperator === operator.MULTIPLY)
+{
+    result = num1 * num2;
+    console.log(chalk.black.bgRedBright(`Result is : ${result}`))
+}else if (selectedOperator === operator.DIVIDE)
+{
+    result = num1 / num2;
+    console.log(chalk.blue.bgRedBright(`Result is : ${result}`))
+}
+
+}
+
+main()
